50398e794c12d66d45cf49095f942c8e03b93a55
Benoît <benoit@m.aurice.net>
2024-07-12 18:47:25 +0200

    Soap: add samples

---
+++ samples/dependencies/cities.soap/IProvideCities.cs
@@ -0,0 +1,30 @@
+﻿namespace cities.soap;
+
+[DataContract]
+public record GpsCoordinates([property:DataMember]decimal Latitude, [property:DataMember]decimal Longitude);
+
+[DataContract]
+public record City([property:DataMember]string Name, [property:DataMember]GpsCoordinates Coordinates);
+    
+[ServiceContract]
+
+public interface IProvideCities
+{
+    [OperationContract]
+    Task<City?> FindCity(string searchString);
+}
+
+[ServiceBehavior(IncludeExceptionDetailInFaults = true)]
+public class ProvideCities : IProvideCities
+{
+    public async Task<City?> FindCity(string searchString)
+    {
+        return searchString switch
+        {
+            "Berlin" => new City("Berlin", new GpsCoordinates(52.5200m, 13.4050m)),
+            "Paris" => new City("Paris", new GpsCoordinates(48.8566m, 2.3522m)),
+            "NewYork" => new City("New York", new GpsCoordinates(40.7128m, 74.0060m)),
+            _ => null
+        };
+    }
+}
---
+++ samples/dependencies/cities.soap/Program.cs
@@ -0,0 +1,19 @@
+﻿
+var builder = WebApplication.CreateBuilder();
+
+builder.Services.AddServiceModelServices();
+builder.Services.AddServiceModelMetadata();
+builder.Services.AddSingleton<IServiceBehavior, UseRequestHeadersForMetadataAddressBehavior>();
+
+var app = builder.Build();
+
+app.UseServiceModel(serviceBuilder =>
+{
+    serviceBuilder.AddService<ProvideCities>();
+    serviceBuilder.AddServiceEndpoint<ProvideCities, IProvideCities>(new BasicHttpBinding(BasicHttpSecurityMode.Transport), "/Cities.svc");
+    var serviceMetadataBehavior = app.Services.GetRequiredService<ServiceMetadataBehavior>();
+    serviceMetadataBehavior.HttpsGetEnabled = true;
+    
+});
+
+app.Run();
